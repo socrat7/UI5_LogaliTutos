58ba02b6f74a25f93d7bbf23688e6a378348d2b0
49157 <49157@Desktop>
2020-10-12 22:53:34 +0200

    LOGALI: 5.3. SAP UI5 Control

---
+++ webapp/index.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+sap.ui.define( [
+		"sap/m/Text"
+	],
+	function(Text) {
+		
+		new Text({
+			text: "Hello World"
+		}).placeAt("content");
+		
+	});
---
+++ webapp/sapui5/view1.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("sapui5.view1", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf sapui5.view1
+	*/ 
+	getControllerName : function() {
+		return "sapui5.view1";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf sapui5.view1
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
