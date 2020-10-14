deeda5c74a2735d1d816b95c582dc1a890dade02
49157 <49157@Desktop>
2020-10-14 22:14:06 +0200

    7. Interfaz de usuario – Objetos/7.4. Fragmentos – Devolución de llamada -> Dialogo en fragmento

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,9 +2,10 @@ sap.ui.define([
 
 		"sap/ui/core/mvc/Controller", //asignamos el controller
 		"sap/m/MessageBox",
-		"sap/m/MessageToast"
+		"sap/m/MessageToast",
+		"sap/ui/core/Fragment"
 	],
-	function (Controller, MessageBox, MessageToast ) { //se paso al Component.js: models, ResourceModel
+	function (Controller, MessageBox, MessageToast, Fragment) { //se paso al Component.js: models, ResourceModel
 
 		"use strict"; //se especifica que es la zona privada del controlador
 
@@ -38,6 +39,38 @@ sap.ui.define([
 				);
 
 				MessageToast.show(sMsg);
+			},
+
+			onOpenDialog: function () {
+
+				//pasamos el identificador de la vista - en que vista queremos cargar el fragmento
+				var oView = this.getView();
+
+				if (!this.byId("helloDialog")) {
+					//cargamos el fragmento para el boton del dialogo
+					Fragment.load({
+
+						id: oView.getId(),
+						name: "sapui5.view.HelloDialog",
+						controller: this //cargamos el mismo controlador al fragmento
+
+					}).then(function (oDialog) {
+
+						//necesitamos conectar el dialogo con la vista
+						oView.addDependent(oDialog);
+						oDialog.open();
+					});
+
+				} else {
+					this.byId("helloDialog").open();
+				}
+			},
+
+			onCloseDialog: function () {
+
+				//tenemos que cerra el dialogo
+				this.byId("helloDialog").close();
+
 			}
 
 		});
