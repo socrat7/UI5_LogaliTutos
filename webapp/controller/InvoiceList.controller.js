e3b6dbfe161312bdfd8bbb27f2e36e07e32c86c5
49157 <49157@Desktop>
2020-10-17 01:58:15 +0200

    8. Binding y Formateadores/8.4. Formateador personalizado

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller", //controlador
-	"sap/ui/model/json/JSONModel" //model
+	"sap/ui/model/json/JSONModel", //model
+	"sapui5/model/formatter"
 	
-	], function(Controller, JSONModel){
+	], function(Controller, JSONModel, formatter){
 		
 		return Controller.extend("sapui5.controller.InvoiceList", {
+			
+			formatter: formatter, //agregar los textos del i18n para el estatus y formatter.js del model
 			
 			onInit : function(){
 				
