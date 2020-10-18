affb690d8935707e7892317f33e3e2aace5538dc
49157 <49157@Desktop>
2020-10-18 19:09:21 +0200

    13. Controles personalizados: -Creación -Metadatos definición -Inicialización -Renderizador -Estilos -Utilización -Modificaciones --> Sistema de valoración de rating asignando estrellas de acuerdo a una puntuación

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -2,10 +2,11 @@ sap.ui.define([
 
 		"sap/ui/core/mvc/Controller", //asignamos el controller
 		"sap/ui/core/routing/History", //agregamos el historial para la navegacion entre vistas, por ejemplo back
-		"sap/ui/core/UIComponent"
-		
+		"sap/ui/core/UIComponent",
+		"sap/m/MessageToast"
+
 	],
-	function (Controller, History, UIComponent) { //se paso al Component.js: models, ResourceModel
+	function (Controller, History, UIComponent, MessageToast) { //se paso al Component.js: models, ResourceModel
 
 		"use strict"; //se especifica que es la zona privada del controlador
 
@@ -21,6 +22,8 @@ sap.ui.define([
 
 			_onObjectMatched: function (oEvent) {
 
+				this.byId("rating").reset();
+
 				this.getView().bindElement({
 
 					//Vamos a hacer el binding de los datos pasados mediante la URI hacia la vista Details"
@@ -29,24 +32,33 @@ sap.ui.define([
 
 				});
 			},
-			
-			onNavBack : function(){
-				
+
+			onNavBack: function () {
+
 				var oHistory = History.getInstance();
 				//agregamos la ruta anterior
 				var sPreviousHash = oHistory.getPreviousHash();
 				//nos aseguramos que exista la ruta anterior
-				if ( sPreviousHash !== undefined) {
-					
+				if (sPreviousHash !== undefined) {
+
 					window.history.go(-1); //si hay historial navega al anterior
-					
-				}else {
-					
+
+				} else {
+
 					//capturamos el router
-					var oRouter = UIComponent.getRouterFor(this); 
+					var oRouter = UIComponent.getRouterFor(this);
 					oRouter.navTo("overview", {}, true); // si no hay historia navega al overview
-					
+
 				}
+			},
+
+			onRatingChange: function (oEvent) {
+
+				var fValue = oEvent.getParameter("value");
+				var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+				MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+
 			}
 		});
 	});
