f29eb236d3d8a0b289cbd38dd6c1288726d9b63b
socrat7 <40124536+socrat7@users.noreply.github.com>
2020-10-18 20:58:43 +0200

    14. Adaptaciones/14.2. System Device: hacer d eun menu desplegable cuando el dispositivo de salida es celular

--- webapp/Component.js
+++ webapp/Component.js
@@ -3,9 +3,13 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sapui5/model/models", //Accedemos el model.js definido en la carpeta model
 	"sap/ui/model/resource/ResourceModel", // para agregar los datos del i18n
-	"sapui5/controller/HelloDialog"
+	"sapui5/controller/HelloDialog",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device",
+	"sap/ui/model/BindingMode"
+	
 
-], function (UIComponent, models, ResourceModel, HelloDialog) {
+], function (UIComponent, models, ResourceModel, HelloDialog, JSONModel, Device, BindingMode) {
 
 	return UIComponent.extend("sapui5.Component", {
 
@@ -30,15 +34,20 @@ sap.ui.define([
 			//set i18n model on view
 			var i18nModel = new ResourceModel({
 				bundleName: "sapui5.i18n.i18n" //la ruta es sapui5 en este caso
-			});
+			}); 
 
 			//cargamos los datos en el modelo
 			this.setModel(i18nModel, "i18n"); // con ésto ya podemos utilizar en i18n en la vista
+			
+			//Responsive - set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode(BindingMode.OneWay);
+			this.setModel(oDeviceModel, "device");
 
 			//cargamos el controlador para el mensaje de dialogo
 			//accedemos a la logica de HelloDialog.js
 			this._helloDialog = new HelloDialog(this.getRootControl());
-			
+
 			//create the views based on the url/hash
 			this.getRouter().initialize();
 
@@ -48,9 +57,9 @@ sap.ui.define([
 			this._helloDialog.destroy();
 			delete this._helloDialog;
 		},
-		
-		openHelloDialog: function(){
-				this._helloDialog.open();
+
+		openHelloDialog: function () {
+			this._helloDialog.open();
 		}
 
 	});
